a505b08dd4dc7f29b4e675c7f9af35fe5df6b843
priyanka897-star <priyankamore8997@gmail.com>
2021-09-30 00:05:21 +0530

    Added:Functionality to Print Fibnocci Series

--- FibnocciSeries.java
+++ FibnocciSeries.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class FibnocciSeries {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter a Number  ");
+		System.out.println("Enter a Number");
 		int number = scanner.nextInt();
 		int number1 = 0;
 		int number2 = 1;
