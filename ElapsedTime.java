1cb4a807f00d9380f801af285cc0cab5e41380f8
priyanka897-star <priyankamore8997@gmail.com>
2021-09-29 23:22:09 +0530

    Added:Files

---
+++ ElapsedTime.java
@@ -0,0 +1,19 @@
+package logicalprograms;
+
+import java.time.temporal.ChronoUnit;
+
+import java.time.*;
+
+public class ElapsedTime {
+	public static void main(String[] args) {
+		LocalTime StartTime = LocalTime.of(19, 21, 40);
+		LocalTime StopTime = LocalTime.of(21, 22, 00);
+		long hours = ChronoUnit.HOURS.between(StartTime, StopTime);
+		long minutes = ChronoUnit.MINUTES.between(StartTime, StopTime);
+		long seconds = ChronoUnit.SECONDS.between(StartTime, StopTime);
+		long toConvertInMinutes = minutes % 6;
+		long toconvertInsecond = seconds % 60;
+		System.out.println("Difference is " + hours + " hours " + toConvertInMinutes + " minutes " + toconvertInsecond
+				+ " seconds.");
+	}
+}
