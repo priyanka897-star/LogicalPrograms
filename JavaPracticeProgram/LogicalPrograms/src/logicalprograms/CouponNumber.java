dc0cda739504fb258a4495ae20c1ea1e706d5237
priyanka897-star <priyankamore8997@gmail.com>
2021-10-01 02:01:20 +0530

    Refactored Content of code

---
+++ JavaPracticeProgram/LogicalPrograms/src/logicalprograms/CouponNumber.java
@@ -0,0 +1,32 @@
+package logicalprograms;
+
+import java.util.Scanner;
+
+public class CouponNumber {
+	// return a random coupon between 0 and n-1
+	public static int coupon(int n) {
+		return (int) (Math.random() * n);
+    }
+
+   public static void main(String[] args) {
+		System.out.println("Enter a card Number to Draw: ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		scanner.close();
+		boolean[] collect = new boolean[number];// collect[i] = true if card type i already collected
+		int count = 0;
+		int notSame = 0;
+		// for number of cards you collect before obtaining one of each of the n types
+		while (notSame < number) {
+			int card = coupon(number);
+			count++;
+			if (!collect[card]) {
+				notSame++;
+				collect[card] = true;
+			}
+		}
+		System.out.println("Total Random Numbers needed: " + count);
+
+	}
+
+}
---
+++ JavaPracticeProgram/LogicalPrograms/src/logicalprograms/FindPerfectNumber.java
@@ -0,0 +1,33 @@
+package logicalprograms;
+
+import java.util.Scanner;
+
+public class FindPerfectNumber {
+
+	// function that checks if the given number is perfect or not
+	public static int isPerfect(int num) {
+		int sum = 0;
+		for (int i = 1; i <= num / 2; i++) {
+			if (num % i == 0) {
+				// calculates the sum of factors
+				sum = sum + i;
+			}
+		}
+
+		return sum;
+	}
+   public static void main(String args[]) {
+		int number, s;
+		Scanner scanner  = new Scanner(System.in);
+		System.out.print("Enter the number: ");
+		number = scanner.nextInt();
+		scanner.close();
+		// calling the function
+		s = isPerfect(number);
+		if (s  == number) {
+			System.out.println(number +" "+ "is Perfect Number");
+		} else {
+			System.out.println(number +" " + "is not a Perfect Number");
+		}
+	}
+}
